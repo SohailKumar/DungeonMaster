ca6636abe1645c37218a476c14c98cacf151d461
John Huggins <92586058+PrvCaboose@users.noreply.github.com>
2023-07-07 19:30:55 -0400

    created GridDemo Scene

---
+++ DungeonMaster/Assets/Scripts/GridManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int rows;
+    int columns;
+    float tileSize;
+    [SerializeField] GameObject tile;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
