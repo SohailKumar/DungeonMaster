105a2280b35407d6d05a3626fa00a437838a9bd4
SohailK <sohail.kumar03@gmail.com>
2023-07-07 20:51:13 -0400

    Implemented GenericSingleton.

---
+++ DungeonMaster/Assets/Scripts/CurrencySystem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrencySystem : GenericSingleton<CurrencySystem>
+{
+    public static int money;
+
+}
