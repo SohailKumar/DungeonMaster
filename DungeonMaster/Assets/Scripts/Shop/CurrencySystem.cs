25b40f550812ba9af8c5e95cf9a3e276c6ad596e
SohailK <sohail.kumar03@gmail.com>
2023-07-08 13:08:58 -0400

    BattlerScene Shop system

--- DungeonMaster/Assets/Scripts/Shop/CurrencySystem.cs
+++ DungeonMaster/Assets/Scripts/Shop/CurrencySystem.cs
@@ -1,10 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CurrencySystem : GenericSingleton<CurrencySystem>
 {
     private static int money;
+    public List<Items> items = new List<Items>();
+    public static TextMeshProUGUI currencyText;
+
+    public void OnLevelWasLoaded(int level)
+    {
+        if (InventoryManager.Instance != null)
+        {
+            InventoryManager.Instance.items = items;
+            InventoryManager.Instance.ListItems();
+        }
+    }
 
     public void addMoney(int addAmount)
     {
@@ -13,7 +25,7 @@ public class CurrencySystem : GenericSingleton<CurrencySystem>
 
     public bool loseMoney(int loseAmount)
     {
-        if(money >= loseAmount)
+        if (money >= loseAmount)
         {
             money -= loseAmount;
             return true;
@@ -24,6 +36,11 @@ public class CurrencySystem : GenericSingleton<CurrencySystem>
     public int getMoney()
     {
         return money;
+    }
+
+    public void setMoneyText()
+    {
+        currencyText.text = "Money: $"+money.ToString();
     }
 
 }
