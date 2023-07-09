236278d2a09e82733fa2261878815fe5c03214d3
Aaron Bush <89756454+Awesomecake@users.noreply.github.com>
2023-07-09 13:31:30 -0400

    Fixed Tweaked

--- DungeonMaster/Assets/Scripts/Trap.cs
+++ DungeonMaster/Assets/Scripts/Trap.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditorInternal;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Trap", menuName = "Trap/Create New Trap")]
