f730ea7fb48122d39e7c0b4b4b650ad38087a092
John Huggins <92586058+PrvCaboose@users.noreply.github.com>
2023-07-08 23:53:57 -0400

    tweaked colors

--- DungeonMaster/Assets/Scripts/PedestalSlot.cs
+++ DungeonMaster/Assets/Scripts/PedestalSlot.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using TMPro;
 
 public class PedestalSlot : MonoBehaviour, IDropHandler
 {
@@ -34,6 +35,7 @@ public class PedestalSlot : MonoBehaviour, IDropHandler
                 {
                     draggableItem.parentAfterDrag = transform;
                     draggableItem.GetComponent<Image>().color = new Color(draggableItem.GetComponent<Image>().color.r, draggableItem.GetComponent<Image>().color.g, draggableItem.GetComponent<Image>().color.b, 0);
+                    //draggableItem.GetComponent<TextMeshPro>().color = new Color(255, 255, 255, 0);
                 }
 
                 // Remove it from inventory
@@ -57,7 +59,7 @@ public class PedestalSlot : MonoBehaviour, IDropHandler
                 {
                     draggableItem.parentAfterDrag = transform;
                     draggableItem.GetComponent<Image>().color = new Color(draggableItem.GetComponent<Image>().color.r, draggableItem.GetComponent<Image>().color.g, draggableItem.GetComponent<Image>().color.b, 255);
-
+                    //draggableItem.GetComponent<TextMeshPro>().color = new Color(255, 255, 255, 225);
                 }
 
                 if (dropped.GetComponent<ItemController>().isTrap && dropped.GetComponent<DraggableItem>().isRemoved)
@@ -78,6 +80,8 @@ public class PedestalSlot : MonoBehaviour, IDropHandler
                 {
                     draggableItem.parentAfterDrag = transform.GetChild(0).GetChild(0);
                     draggableItem.GetComponent<Image>().color = new Color(draggableItem.GetComponent<Image>().color.r, draggableItem.GetComponent<Image>().color.g, draggableItem.GetComponent<Image>().color.b, 255);
+                    //draggableItem.GetComponent<TextMeshPro>().color = new Color(255, 255, 255, 255);
+
 
                     Debug.Log("InvDriop");
                 }
