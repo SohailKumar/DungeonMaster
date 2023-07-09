3541af5380baf22edc73ccd776e7cd730bf9302f
John Huggins <92586058+PrvCaboose@users.noreply.github.com>
2023-07-08 22:37:07 -0400

    Getting defense to work

--- DungeonMaster/Assets/Scripts/NPCGoal.cs
+++ DungeonMaster/Assets/Scripts/NPCGoal.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class NPCGoal : MonoBehaviour
 {
+    [SerializeField] public List<GameObject> defenses = new List<GameObject>();
+    public int addDefense = 0;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("THINGERMEBAB: " + collision.gameObject.name);
         if (collision.CompareTag("Adventurer"))
         {
             collision.GetComponent<NPCAdventurer>().Die();
-        } 
+
+            if (20 - addDefense < 0)
+            {
+                addDefense = 0;
+            }
+            CurrencySystem.Instance.loseMoney(20 - addDefense);
+            Debug.Log("Money: " + CurrencySystem.Instance.getMoney());
+        }
+    }
+
+    public void OnStart()
+    {
+        Debug.Log("thing");
+        foreach (GameObject obj in defenses)
+        {
+            if (obj.transform.childCount > 0)
+            {
+                addDefense += obj.GetComponentInChildren<ItemController>().Item.Defense;
+            }
+        }
     }
 }
